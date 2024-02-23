7ecaacd07de9e43de9317501062ddf2ff9c35371
Le Van Hieu <hieulv.2305@gmail.com>
2024-02-24 02:45:27 +0700

    done breakcrumb Reports Center

--- src/pages/admin/Reports/components/ReviewsCenter/index.tsx
+++ src/pages/admin/Reports/components/ReviewsCenter/index.tsx
@@ -1,5 +1,26 @@
+import { Breadcrumb } from 'antd';
+import { Link } from 'react-router-dom';
 const ReviewsCenter = () => {
-  return <div>ReviewsCenter</div>;
+  return (
+    <div>
+      <div className='breakcrumb'>
+        <Breadcrumb
+          items={[
+            {
+              title: 'Reports Center'
+            },
+            {
+              title: 'Exams'
+            },
+            {
+              title: <Link to='#'>Review center</Link>
+            }
+          ]}
+        />
+      </div>
+      ReviewsCenter
+    </div>
+  );
 };
 
 export default ReviewsCenter;
